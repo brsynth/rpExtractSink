ff1cefa16a8bbabe55bd95e9b7b79f81d4b8e500
joan <joan.herisson@univ-evry.fr>
2020-10-14 12:34:27 +0200

    fix(case): lowercase source filenames
    PyPi seems to lowercase filenames of sources

--- rpextractsink/__init__.py
+++ rpextractsink/__init__.py
@@ -6,6 +6,6 @@ Created on Oct 5 2020
 """
 
 
-from rpextractsink.rpExtractSink import rpExtractSink, build_args_parser
+from rpextractsink.rpextractsink import rpExtractSink, build_args_parser
 
 __all__ = ["rpExtractSink", "build_args_parser"]
