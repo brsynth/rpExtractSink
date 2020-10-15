8f63e9c588559c7778006bb14b24d92a47cedaaf
joan <joan.herisson@univ-evry.fr>
2020-10-15 10:45:50 +0200

    refactor(only conda): modify to only publish a pure conda package

--- extras/build.sh
+++
@@ -1,4 +0,0 @@
-conda update -y -n base -c defaults conda
-conda env create -n build -f extras/environment.yml
-conda run -n build python setup.py bdist_conda
-if errorlevel 1 exit 1
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
+# from distutils.core import setup, Extension
+# import distutils.command.bdist_conda
 
 _readme = 'README.md'
+
 with open(_readme, 'r') as f:
     long_description = f.read()
 
 _release = 'RELEASE'
+
 with open(_release, 'r') as f:
     _version = f.readline().split()[0]
 
@@ -21,34 +25,38 @@ with open(_extras_path+'/.env', 'r') as f:
             _descr = line.splitlines()[0].split('=')[1].lower()
         if line.startswith('CORR_AUTHOR='):
             _corr_author = line.splitlines()[0].split('=')[1].lower()
+
 from yaml import safe_load as yaml_safe_load
 from yaml import YAMLError
 required = []
-with open(_extras_path+'/environment-run.yml', 'r') as stream:
+with open(_extras_path+'/environment-run.yml', 'r') as run_stream:
+# with open(_extras_path+'/environment-build.yml', 'r') as build_stream:
     try:
-        required = yaml_safe_load(stream)['dependencies']
+        required = yaml_safe_load(run_stream)['dependencies']
+        # required += yaml_safe_load(build_stream)['dependencies']
     except yaml.YAMLError as exc:
         print(exc)
 
+
 setup(
-    name=_package,
-    version=_version,
-    author=_authors,
-    author_email=_corr_author,
-    description=_descr,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url=_url,
-    packages=[_package],
-    package_dir={_package: _package},
-    include_package_data=True,
-    install_requires=required,
-    test_suite='pytest',
-    license='MIT',
+    name                          = _package,
+    version                       = _version,
+    author                        = _authors,
+    author_email                  = _corr_author,
+    description                   = _descr,
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    url                           = _url,
+    packages                      = [_package],
+    package_dir                   = {_package: _package},
+    include_package_data          = True,
+    # install_requires              = required,
+    test_suite                    = 'pytest',
+    license                       = 'MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.5',
+    python_requires               = '>=3.5',
 )
